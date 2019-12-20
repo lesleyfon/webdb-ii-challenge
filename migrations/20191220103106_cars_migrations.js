9652f1beca0f84d6680b71f38ac60c0f012523dd
lesley <lesleybanadzem2017@gmail.com>
2019-12-20 10:59:20 -0800

    Created db with knex and migrations and build the schema for the cars table

---
+++ migrations/20191220103106_cars_migrations.js
@@ -0,0 +1,14 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable('cars', table => {
+        table.increments('id').primary();
+        table.text('VIN', 17).unique().notNullable();
+        table.text('make', 128 ).notNullable();
+        table.text('model').notNullable();
+        table.integer('mileage').notNullable();
+    });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTable('cars')
+};
